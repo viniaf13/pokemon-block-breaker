7439fc754df60f7aaaf36a1965be3045fd3d2deb
viniaf13 <viniaf13@gmail.com>
2020-05-07 19:11:28 -0300

    block count part 1

--- Pokemon Block Breaker/Assets/Scripts/Block.cs
+++ Pokemon Block Breaker/Assets/Scripts/Block.cs
@@ -6,9 +6,17 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    //Cache reference
+    Level level; 
+    
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 0.2f);
+        level.DecreaseTotalBlockNumber();
         Destroy(gameObject);
     }
 
---
+++ Pokemon Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //Parameters
+    int breakableBlocks;
+
+    //Cached reference
+    SceneLoader sceneLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        breakableBlocks = GetTotalBlockNumber();
+        Debug.Log("total: " + breakableBlocks);
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private int GetTotalBlockNumber()
+    {
+        GameObject[] allBlocks = GameObject.FindGameObjectsWithTag("block");
+        return allBlocks.Length;
+    }
+
+    public void DecreaseTotalBlockNumber()
+    {
+        breakableBlocks--;
+        Debug.Log("total: " + breakableBlocks);
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
